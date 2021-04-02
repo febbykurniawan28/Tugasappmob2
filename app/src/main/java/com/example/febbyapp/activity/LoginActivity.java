d311cc741195fcdcd6ca9b13df5e0dfcbb56979c
febbykurniawan28 <febbykurniawan800@gmail.com>
2021-04-02 22:41:54 +0700

    aku commit

---
+++ app/src/main/java/com/example/febbyapp/activity/LoginActivity.java
@@ -0,0 +1,52 @@
+package com.example.febbyapp.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.febbyapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button login;
+    EditText username,password;
+
+    protected void onCreate(Bundle saveInstanceState) {
+
+        super.onCreate(saveInstanceState);
+
+        setContentView(R.layout.login_relative);
+
+        login = (Button) findViewById(R.id.btnLogin);
+        username = (EditText) findViewById(R.id.etUsername);
+        password = (EditText) findViewById(R.id.Password);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (password.getText().toString().toUpperCase().equals("FEBBY"))
+                {
+                    login_sucsess();
+                } else
+                {
+                    Toast.makeText(getApplicationContext(), "Salah Password",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void login_sucsess() {
+        String user_login = username.getText().toString();
+        Intent i = new Intent(this,MainActivity.class);
+        //kirimkan usernamenya ke tampilan utama
+        i.putExtra("USERNAME",user_login);
+        startActivity(i);
+
+    }
+}
